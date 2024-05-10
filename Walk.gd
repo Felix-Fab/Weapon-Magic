95702464301d3983444b73f7b429b899e79ca6bb
Felix-Fab <Info.FelixFab@gmail.com>
2024-05-11 00:28:15 +0200

    Add Walk_Top Assets

--- Walk.gd
+++ Walk.gd
@@ -6,17 +6,25 @@ func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_direction * SPEED
 	
-	if(Input.is_action_pressed("ui_right")):
-		$AnimatedSprite2D.play("right")
-		
-	if(Input.is_action_pressed("ui_left")):
-		$AnimatedSprite2D.play("left")
-		
 	if(Input.is_action_pressed("ui_down")):
+		print("Down Pressed")
 		$AnimatedSprite2D.play("down")
+		return
 		
 	if(Input.is_action_pressed("ui_up")):
 		$AnimatedSprite2D.play("up")
+		return
+	
+	if(Input.is_action_pressed("ui_right")):
+		print("Right Pressed")
+		$AnimatedSprite2D.play("right")
+		return
+		
+	if(Input.is_action_pressed("ui_left")):
+		$AnimatedSprite2D.play("left")
+		return
+	
+	$AnimatedSprite2D.play("idle")
 
 func _physics_process(delta):
 	get_input()
