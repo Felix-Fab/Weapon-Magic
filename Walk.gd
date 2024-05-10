66583c3c763603c7249f6bf0c83c7a519aa4d6b5
Felix-Fab <Info.FelixFab@gmail.com>
2024-05-10 23:11:07 +0200

    Add Human Assets

--- Walk.gd
+++ Walk.gd
@@ -5,6 +5,18 @@ const SPEED = 300.0
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_direction * SPEED
+	
+	if(Input.is_action_pressed("ui_right")):
+		$AnimatedSprite2D.play("right")
+		
+	if(Input.is_action_pressed("ui_left")):
+		$AnimatedSprite2D.play("left")
+		
+	if(Input.is_action_pressed("ui_down")):
+		$AnimatedSprite2D.play("down")
+		
+	if(Input.is_action_pressed("ui_up")):
+		$AnimatedSprite2D.play("up")
 
 func _physics_process(delta):
 	get_input()
